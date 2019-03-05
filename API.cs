9fbe31868395016c921f487b3041c3b5867b4657
Tetsu Otter <public@technotter.com>
2019-03-05 19:42:52 +0900

    Add some files

---
+++ API.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR.BIDScs
+{
+  /// <summary>
+  /// Provide the interface of BIDS
+  /// </summary>
+  public class API
+  {
+    /// <summary>
+    /// Initialize the Class
+    /// </summary>
+    public API()
+    {
+
+    }
+  }
+}
